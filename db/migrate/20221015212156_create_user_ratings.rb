a9d005427ce03610c27399604a972735da126f23
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-16 03:41:55 -0400

    created a user rating resource

--- db/migrate/20221015212156_create_user_ratings.rb
+++ db/migrate/20221015212156_create_user_ratings.rb
@@ -3,7 +3,7 @@ class CreateUserRatings < ActiveRecord::Migration[7.0]
     create_table :user_ratings do |t|
       t.string :image
       t.string :name
-      t.float :rating
+      t.float :rating,
       t.string :reason
       t.belongs_to :user, foreign_key: true
 
